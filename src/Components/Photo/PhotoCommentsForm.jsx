72a2343a4e380e8f898ad802a64f854f22394ca8
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-05-22 15:07:17 -0400

    DOGS - diz a interação e estilizei a parte de comentários no post

---
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Enviar from "../../assets/enviar.svg?react";
+import useFetch from "../../Hooks/useFetch";
+import { COMMENT_POST } from "../../api";
+import Error from "../../Components/Helper/Error";
+import styles from "./PhotoCommentsForm.module.css";
+
+const PhotoCommentsForm = ({ id, setComments }) => {
+  const [comment, setComment] = React.useState("");
+  const { request, error } = useFetch();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { url, options } = COMMENT_POST(id, { comment });
+    const { response, json } = await request(url, options);
+    if (response.ok) {
+      setComments((comments) => [...comments, json]);
+      setComment("");
+    }
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="comment">Insira seu comentário:</label>
+        <textarea className={styles.textarea} id="comment" name="comment" value={comment} onChange={({ target }) => setComment(target.value)} />
+      </div>
+      <button className={styles.button}>
+        <Enviar />
+      </button>
+      {error && <Error error={error} />}
+    </form>
+  );
+};
+
+export default PhotoCommentsForm;
--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import styles from "./PhotoContent.module.css";
 import { Link } from "react-router-dom";
-import PhotoComents from "./PhotoComents";
+import PhotoComments from "./PhotoComments";
 
 const PhotoContent = ({ data }) => {
-  const { photo, coments } = data;
+  const { photo, comments } = data;
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
@@ -25,7 +24,7 @@ const PhotoContent = ({ data }) => {
           </ul>
         </div>
       </div>
-      <PhotoComents id={photo.id} comments={coments} />
+      <PhotoComments id={photo.id} comments={comments} />
     </div>
   );
 };
