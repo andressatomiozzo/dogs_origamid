8bbc8b48f3d73c54be4766103d1135fcdbfccc2c
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-05-25 11:34:59 -0400

    DOGS - add o skeleton (animação enquanto a foto carrega) e a animação do loading

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -4,6 +4,7 @@ import PhotoComments from "./PhotoComments";
 import React from "react";
 import UserContext from "../../createContext/UserContext";
 import PhotoDelete from "./PhotoDelete";
+import Image from "../Helper/Image";
 
 const PhotoContent = ({ data }) => {
   const { photo, comments } = data;
@@ -11,7 +12,7 @@ const PhotoContent = ({ data }) => {
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title}/>
       </div>
       <div className={styles.details}>
         <div>
--- src/Pages/Feed/FeedPhotosItem.jsx
+++ src/Pages/Feed/FeedPhotosItem.jsx
@@ -1,3 +1,4 @@
+import Image from "../../Components/Helper/Image";
 import styles from "./FeedPhotosItem.module.css";
 const FeedPhotosItem = ({ photo, setModalPhoto }) => {
   const handleClick = () => {
@@ -5,7 +6,7 @@ const FeedPhotosItem = ({ photo, setModalPhoto }) => {
   };
   return (
     <li className={styles.photo} onClick={handleClick}>
-      <img src={photo.src} alt={photo.title} />
+      <Image src={photo.src} alt={photo.title} />
       <span className={styles.visualizacao}>{photo.acessos}</span>
     </li>
   );
