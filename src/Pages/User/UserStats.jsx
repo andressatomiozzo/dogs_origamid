b9325440c3a25a79c2f0e23dd4fd90c0cba690bb
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-05-28 09:55:37 -0400

    DOGS - otimizei as estatísticas com o lazy e o suspense

--- src/Pages/User/UserStats.jsx
+++ src/Pages/User/UserStats.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Head from "../../Components/Helper/Head";
-import UserStatsGraphs from "./UserStatsGraphs";
 import useFetch from "../../Hooks/useFetch";
 import Loading from "../../Components/Helper/Loading";
 import Error from "../../Components/Helper/Error";
 import { STATS_GET } from "../../api";
+const UserStatsGraphs = React.lazy(() => import("./UserStatsGraphs"))
 
 const UserStats = () => {
   const { loading, error, data, request } = useFetch();
@@ -21,10 +21,10 @@ const UserStats = () => {
   if (error) return <Error error={error} />;
   if (data)
     return (
-      <div>
+      <React.Suspense fallback={<div></div>}>
         <Head title="Estatísticas" />
         <UserStatsGraphs data={data} />
-      </div>
+      </React.Suspense>
     );
   else return null;
 };
